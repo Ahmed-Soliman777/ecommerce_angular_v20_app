ad775f695cc1c858438604c336d6fad25bb5cc52
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-05 01:24:20 +0300

    add category api to index.js file

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv')
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+const categoryRoute = require('./routes/category')
+
+dotenv.config();
+
+app.use(express.json())
+
+app.use('/ecommerce/api', categoryRoute)
+
+app.get('', (req, res) => {
     res.send('Hello World!');
 });
+
+mongoose.connect(process.env.MONGO_URL).then(() => console.log('connected to mongodb server')).catch(err => console.log(err));
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
