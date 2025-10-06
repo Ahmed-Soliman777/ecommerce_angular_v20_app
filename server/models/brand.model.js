9a382db86ed9bd07faca07f48a0555c57888a77b
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-06 19:45:49 +0300

    add schema to brand model

--- server/models/brand.model.js
+++ server/models/brand.model.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
-const categorySchema = new mongoose.Schema({
+const brandSchema = new mongoose.Schema({
     name: { type: String, required: true, unique: true },
 }, { timestamps: true });
-const Category = mongoose.model("categories", categorySchema);
-module.exports = Category;
+const Brand = mongoose.model("categories", brandSchema);
+module.exports = Brand;
