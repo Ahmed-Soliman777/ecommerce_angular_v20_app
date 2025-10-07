5ecd318d7934f0b59f9475023d826ba7548d4be4
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-07 19:40:22 +0300

    add controllers to product ctrl file

---
+++ server/controllers/product.ctrl.js
@@ -0,0 +1,66 @@
+const products = require("../models/product.modle")
+
+async function handleGetProducts(req, res) {
+    try {
+        const getProducts = await products.find()
+        res.status(200).json(getProducts)
+
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+async function handleGetProductById(req, res) {
+    try {
+        const { id } = req.params
+        if (!id) {
+            res.status(400).json({ message: "Product not found" })
+        }
+        const getProduct = await products.findById(id)
+        res.status(200).json(getProduct)
+
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+async function handleAddProducts(req, res) {
+    try {
+        const { name, description, shortDescription, purshasePrice, sellingPrice, images, categoryId } = req.body
+        if (!name || !purshasePrice || !sellingPrice) {
+            res.status(400).json({ message: "Required info missed" })
+        }
+        const addProduct = await products.create({ name, description, shortDescription, purshasePrice, sellingPrice, images, categoryId })
+        res.status(200).json(addProduct)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+async function handleUpdateProducts(req, res) {
+    try {
+        const { id } = req.params
+        if (!id) {
+            res.status(400).json({ message: "Product not found" })
+        }
+        const updateProduct = await products.findByIdAndUpdate(id, { new: true })
+        res.status(200).json(updateProduct)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+async function handleDeleteProducts(req, res) {
+    try {
+        const { id } = req.params
+        if (!id) {
+            res.status(400).json({ message: "Product not found" })
+        }
+        const deleteProduct = await products.findByIdAndDelete(id)
+        res.status(200).json(deleteProduct)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+module.exports = { handleGetProducts, handleGetProductById, handleAddProducts, handleUpdateProducts, handleDeleteProducts }
