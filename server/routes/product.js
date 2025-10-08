9a3253595db495bd792777752e4fda0dd066be50
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-09 00:52:56 +0300

    add new and featured routes

--- server/routes/product.js
+++ server/routes/product.js
@@ -5,6 +5,8 @@ const productCtrl = require('../controllers/product.ctrl')
 
 route.get('/products', productCtrl.handleGetProducts)
 route.get('/product/:id', productCtrl.handleGetProductById)
+route.get('/products/new-product', productCtrl.handleGetNewProducts)
+route.get('/products/featured-product', productCtrl.handleGetFeaturedProducts)
 route.post('/product/add', productCtrl.handleAddProducts)
 route.put('/product/update/:id', productCtrl.handleUpdateProducts)
 route.delete('/products/delete/:id', productCtrl.handleDeleteProducts)
