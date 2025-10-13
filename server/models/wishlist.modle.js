8744d67d0aedbb88238610fbf142034f7e8b34da
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-13 17:59:13 +0300

    add wishlist schema

--- server/models/wishlist.modle.js
+++ server/models/wishlist.modle.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const wishlistSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
-    productId: { type: [string]},
+    productId: { type: mongoose.Schema.Types.ObjectId, ref: "products"},
 }, { timestamps: true });
 const Wishlist = mongoose.model("whishlists", wishlistSchema);
 module.exports = Wishlist;
