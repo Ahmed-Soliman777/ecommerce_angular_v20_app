846210d6d81e4d56ef68c5baf088069568088245
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-13 22:05:45 +0300

    filter wishlist users

--- server/controllers/wishlist.ctrl.js
+++ server/controllers/wishlist.ctrl.js
@@ -2,8 +2,10 @@ const wishlist = require('../models/wishlist.modle')
 
 async function getWishlist(req, res) {
     try {
-        const getWishlist = await wishlist.find()
-        res.status(200).json(getWishlist)
+        const wishlists = await wishlist.find({ userId: req.user.id })
+            .populate("productId")
+            .sort({ createdAt: -1 });
+        res.status(200).json(wishlists)
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
@@ -14,7 +16,7 @@ async function getWishlistById(req, res) {
         if (!id) {
             res.status(400).json({ message: "Wishlist not found" })
         }
-        const getWishlist = await wishlist.findById(id)
+        const getWishlist = await wishlist.findById(id).populate("productId");
         res.status(200).json(getWishlist)
     } catch (error) {
         res.status(500).json({ message: error.message })
@@ -22,9 +24,14 @@ async function getWishlistById(req, res) {
 }
 async function addWishlist(req, res) {
     try {
-        const { userId, productId } = req.body
+        const userId = req.user.id;
+        const { productId } = req.body
         if (!userId || !productId) {
             res.status(400).json({ message: "Invalid user or product ID" })
+        }
+        const existing = await wishlist.findOne({ userId, productId });
+        if (existing) {
+            return res.status(400).json({ message: "Product already in wishlist" });
         }
         const getWishlist = await wishlist.create({ userId, productId })
         res.status(200).json({ message: "Wishlist added successfully", getWishlist })
@@ -39,7 +46,10 @@ async function deleteWishlist(req, res) {
         if (!id) {
             res.status(400).json({ message: "Invalid product ID" })
         }
-        const getWishlist = await wishlist.findByIdAndDelete(id)
+        const deleteItem = await wishlist.findByIdAndDelete(id)
+        if (!deleteItem) {
+            return res.status(404).json({ message: "Wishlist not found" });
+        }
         res.status(200).json({ message: "Wishlist deleted successfully" })
     } catch (error) {
         res.status(500).json({ message: error.message })
