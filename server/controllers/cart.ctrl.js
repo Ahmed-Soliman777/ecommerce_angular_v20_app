866f65ef74efefce08ab7b2c3e15034ea21c6b96
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-14 21:27:16 +0300

    add cart controllers

--- server/controllers/cart.ctrl.js
+++ server/controllers/cart.ctrl.js
@@ -18,7 +18,45 @@ async function handleRemoveFromCart(userId, productId) {
     await Cart.findOneAndDelete({ userId: userId, productId: productId })
 }
 
-async function handleGetCart(userId, productId) {
-    const product = await Cart.find({ userId: userId }).populate('productId')
-    return product.map(x => x.productId)
+async function handleGetCart(userId) {
+    const product = await Cart.find({ userId: userId }).populate("productId")
+    return product.map((x) => {
+        return { quantity: x.quantity, product: x.productId }
+    })
 }
+
+async function getCart(req, res) {
+    try {
+        const { userId } = req.body
+        const items = await handleGetCart(userId)
+        res.status(200).json(items)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+
+async function addCart(req, res) {
+    try {
+        const productId = req.params.id // product id
+        const userId = req.user._id
+        const { quantity } = req.body
+        const items = await handleAddToCart(userId, productId, quantity)
+        res.status(200).json(items)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+async function removeCart(req, res) {
+    try {
+        const productId = req.params.id
+        const userId = req.user._id
+        const items = await handleRemoveFromCart(userId, productId)
+        res.status(200).json(items)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+module.exports = { getCart, addCart, removeCart }
