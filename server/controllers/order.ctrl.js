d53bd54d7651002872e140bfdcd6bfdc14f5d15b
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-15 15:41:56 +0300

    add handle getOrders controller

--- server/controllers/order.ctrl.js
+++ server/controllers/order.ctrl.js
@@ -1,4 +1,5 @@
 const Order = require('../models/orders.modle')
+const { handleClearCart } = require('./cart.ctrl')
 
 async function handleAddOrder(userId, orderModel) {
     let order = new Order({
@@ -19,6 +20,7 @@ async function addOrders(req, res) {
         const userId = req.user.id
         const order = req.body
         await handleAddOrder(userId, order)
+        await handleClearCart(userId)
         res.status(200).json({ message: "order created successfully" })
     } catch (error) {
         res.status(500).json({ message: error.message })
