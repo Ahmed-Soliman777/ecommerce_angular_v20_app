c456e5f8a0acff21e9a3971f1d7f3ecc988e097c
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-15 12:52:02 +0300

    add user id ref to orders model

--- server/models/orders.modle.js
+++ server/models/orders.modle.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
     date: { type: Date, default: Date.now },
     items: Array(mongoose.Schema.Types.Mixed),
     paymentType: { type: String },
