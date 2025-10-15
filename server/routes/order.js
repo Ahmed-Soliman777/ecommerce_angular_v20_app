f589550f5f5280748f04d01afddaface28c273cb
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-15 19:37:48 +0300

    add update order route

--- server/routes/order.js
+++ server/routes/order.js
@@ -7,5 +7,6 @@ const middleware = require('../middleware/auth.middleware')
 route.post('/order', middleware.authenticate, OrderCtrl.addOrders)
 route.get('/order', middleware.authenticate, OrderCtrl.getOrders)
 route.get('/admin/order', middleware.authenticate, middleware.isAdmin, OrderCtrl.getAllOrders)
+route.post('/admin/order/:id', middleware.authenticate, middleware.isAdmin, OrderCtrl.updateOrder)
 
 module.exports = route
