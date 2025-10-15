238fbe84aef6270a8b793726130a4767fd683645
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-10-15 15:42:32 +0300

    make status type string

--- server/models/orders.modle.js
+++ server/models/orders.modle.js
@@ -6,7 +6,7 @@ const orderSchema = new mongoose.Schema({
     items: Array(mongoose.Schema.Types.Mixed),
     paymentType: { type: String },
     address: mongoose.Schema.Types.Mixed,
-    status: { type: Number },
+    status: { type: String },
 }, { timestamps: true });
 const Order = mongoose.model("orders", orderSchema);
 module.exports = Order;
